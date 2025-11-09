fb07730fe346e16dd746be8ff6e87c30eed9f3b9
folajindayo <temidayofolajin@yahoo.com>
2025-11-09 05:34:46 +0100

    Add airdrop registry data with project criteria

---
+++ packages/shared/data/index.ts
@@ -0,0 +1,7 @@
+import airdropsData from './airdrops.json';
+import type { AirdropProject } from '../types';
+
+export const AIRDROPS: AirdropProject[] = airdropsData as AirdropProject[];
+
+export { airdropsData };
+
