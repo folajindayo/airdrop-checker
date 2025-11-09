c942e29be8f7a0760bffdfd838ada23ff9967966
folajindayo <temidayofolajin@yahoo.com>
2025-11-09 05:34:01 +0100

    Add shared constants for chains and configuration

---
+++ packages/shared/constants/index.ts
@@ -0,0 +1,17 @@
+export * from './chains';
+
+export const CACHE_TTL = {
+  AIRDROP_CHECK: 60 * 60 * 1000, // 1 hour
+  AIRDROPS_LIST: 5 * 60 * 1000, // 5 minutes
+  GOLDRUSH_DATA: 60 * 60 * 1000, // 1 hour
+};
+
+export const RATE_LIMITS = {
+  REFRESH_COOLDOWN: 5 * 60 * 1000, // 5 minutes
+};
+
+export const API_CONFIG = {
+  GOLDRUSH_BASE_URL: 'https://api.goldrush.dev/v1',
+  REQUEST_TIMEOUT: 10000, // 10 seconds
+};
+
