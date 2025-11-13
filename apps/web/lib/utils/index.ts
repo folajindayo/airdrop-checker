306101e10e2bd1c5a386f0c862d5c20ae1f4978d
folajindayo <temidayofolajin@yahoo.com>
2025-11-13 01:01:47 +0100

    refactor(utils): create utility index
    - Centralize web app utility exports
    - Export HTTP client, request/response handlers
    - Export query builder, storage, and logger

---
+++ apps/web/lib/utils/index.ts
@@ -0,0 +1,11 @@
+/**
+ * Centralized utility exports
+ */
+
+export * from './http-client';
+export * from './response-handlers';
+export * from './request-helpers';
+export * from './query-builder';
+export * from './storage';
+export * from './logger';
+
