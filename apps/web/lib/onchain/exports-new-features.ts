02bdd6ea3467e09a8c5a070b71194cf8ff43b7ac
seyi.idowu <shezi357412@icloud.com>
2025-11-17 20:27:42 +0100

    Update exports for latest 30 onchain features with Reown

--- apps/web/lib/onchain/exports-new-features.ts
+++ apps/web/lib/onchain/exports-new-features.ts
@@ -1,5 +1,5 @@
 /**
- * Export file for latest onchain features (768-797)
+ * Export file for latest onchain features (798-827)
  * Centralized exports for easy importing
  */
 
@@ -8,8 +8,16 @@ export * from './utils-new-features';
 export * from './helpers-new-features';
 export * from './validation-new-features';
 export * from './errors-new-features';
+export * from './calculators-new-features';
+export * from './formatters-new-features';
+export * from './filters-new-features';
+export * from './aggregators-new-features';
+export * from './transformers-new-features';
+export * from './mappers-new-features';
+export * from './sorters-new-features';
+export * from './config-new-features';
 
 export const LATEST_FEATURES_COUNT = 30;
-export const LATEST_FEATURES_RANGE = '768-797';
-export const TOTAL_FEATURES = 830;
+export const LATEST_FEATURES_RANGE = '798-827';
+export const TOTAL_FEATURES = 860;
 
