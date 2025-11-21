e1303acf1109220b67b473e81630457bcd9dbaae
folajindayo <temidayofolajin@yahoo.com>
2025-11-21 12:53:33 +0100

    chore: add use cases barrel export

--- apps/web/src/domain/use-cases/index.ts
+++ apps/web/src/domain/use-cases/index.ts
@@ -1,7 +1,9 @@
 /**
- * Domain Use Cases Barrel Export
+ * Use Cases Barrel Export
  */
 
-export * from './check-airdrop-eligibility.use-case';
-export * from './get-wallet-airdrops.use-case';
-
+export { CheckEligibilityUseCase } from './check-eligibility.use-case';
+export { GetActiveAirdropsUseCase } from './get-active-airdrops.use-case';
+export { GetWalletPortfolioUseCase } from './get-wallet-portfolio.use-case';
+export { ClaimAirdropUseCase } from './claim-airdrop.use-case';
+export { SearchAirdropsUseCase } from './search-airdrops.use-case';
