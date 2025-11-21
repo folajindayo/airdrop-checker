75b98f6019a4c321186ec077b328ced38b0f7841
folajindayo <temidayofolajin@yahoo.com>
2025-11-21 12:29:53 +0100

    chore: add Prettier configuration

---
+++ apps/web/.prettierrc.js
@@ -0,0 +1,13 @@
+/**
+ * Prettier Configuration
+ */
+
+module.exports = {
+  semi: true,
+  trailingComma: 'es5',
+  singleQuote: true,
+  printWidth: 100,
+  tabWidth: 2,
+  endOfLine: 'lf',
+};
+
