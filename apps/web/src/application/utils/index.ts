3790de5ea08fed4d937e8b9403396782674b8f76
folajindayo <temidayofolajin@yahoo.com>
2025-11-21 12:53:33 +0100

    chore: add utils barrel export

---
+++ apps/web/src/application/utils/index.ts
@@ -0,0 +1,10 @@
+/**
+ * Utils Barrel Export
+ */
+
+export * from './amount-formatter.util';
+export * from './date-formatter.util';
+export * from './percentage.util';
+export * from './contract.util';
+export * from './url.util';
+
