551ee95a5eeb135bbafec8450f978edef3be244d
folajindayo <temidayofolajin@yahoo.com>
2025-11-21 13:03:03 +0100

    chore: add services barrel export

---
+++ apps/web/src/application/services/index.ts
@@ -0,0 +1,9 @@
+/**
+ * Services Barrel Export
+ */
+
+export { airdropEligibilityService } from './airdrop-eligibility.service';
+export { cacheService } from './cache.service';
+export { analyticsService } from './analytics.service';
+export { web3Service } from './web3.service';
+
